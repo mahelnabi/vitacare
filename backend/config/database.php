eab7a2d8ceaeb30738cc06d0e7b5db72856c641f
josselintual <josselin.tual@edu.ece.fr>
2026-05-31 16:38:04 +0200

    Update database.php

--- backend/config/database.php
+++ backend/config/database.php
@@ -3,7 +3,7 @@ define('DB_HOST', 'localhost');
 define('DB_PORT', '3306');
 define('DB_NAME', 'vitacare');
 define('DB_USER', 'root');
-define('DB_PASS', 'root');
+define('DB_PASS', 'root');  
  
 function getDB() {
     try {
